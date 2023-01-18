5e39268fbc45b3692a84089635c9663f31db1909
deadboizxc <yvvncc@gmail.com>
2023-01-18 17:53:43 +0100

    added file sending using ID

--- send.py
+++ send.py
@@ -10,7 +10,8 @@ import os
 async def cmd_(client, message):
     await message.delete()
 
-    files = message.text.split(maxsplit=2)[1]
+    files = message.text.split(maxsplit=2)[2]
+    types = message.text.split(maxsplit=2)[1]
 
     documents = [".py", ".txt", ".sh"]
     audios =[".mp3", ".m4a", ".opus", ".aac"]
@@ -38,11 +39,40 @@ async def cmd_(client, message):
 
     for video in videos:
         if suff == photo:
-            await client.send_photo(chat_id=message.chat.id, photo=files)
+            await client.send_video(chat_id=message.chat.id, video=files)
         else:
             pass
 
 
-modules_help["example"] = {
-    "send": ".send [file name/file_id]",
+@Client.on_message(filters.command("send", prefixes=prefix) & filters.me)
+async def cmd_(client, message):
+    await message.delete()
+
+    files = message.text.split(maxsplit=2)[2]
+    types = message.text.split(maxsplit=2)[1]
+
+
+    if types == "aud":
+        await client.send_audio(chat_id=message.chat.id, audio=files)
+    else:
+        pass
+
+    if types == "doc":
+        await client.send_document(chat_id=message.chat.id, document=files)
+    else:
+        pass
+
+    if types == "photo":
+        await client.send_photo(chat_id=message.chat.id, photo=files)
+    else:
+        pass
+
+    if types == "vid":
+         await client.send_video(chat_id=message.chat.id, video=files)
+    else:
+         pass
+
+
+modules_help["send"] = {
+    "send": ".send [local file name] or .send [types] [file id]",
 }
